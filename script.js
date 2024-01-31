7aa9451743ce5853bcc1735a7a81b9b04bb760cc
VineethGvs <63966915+VineethGvs@users.noreply.github.com>
2024-01-30 16:25:10 -0800

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav a");
+  
+    navLinks.forEach(link => {
+      link.addEventListener("click", smoothScroll);
+    });
+  
+    function smoothScroll(e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      
+      window.scroll({
+        top: targetElement.offsetTop - document.querySelector("nav").offsetHeight,
+        behavior: "smooth"
+      });
+    }
+  });
+
+  function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
